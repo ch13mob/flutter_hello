4d22fb7befd38268017c6528706c1ca925039e7c
ch13mob <ch13mob@gmail.com>
2021-10-18 19:54:38 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/ch13mob/flutter_hello/flutter_hello/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ch13mob.flutter_hello.flutter_hello
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      color: Colors.yellow,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Container(),
+      ),
+    );
+  }
+}
